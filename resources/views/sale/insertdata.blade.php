73d86b539517114107bc5e92c63d0eca30414a32
unknown <devintern3e@gmail.com>
2025-09-05 17:52:41 +0700

    5po

--- resources/views/sale/insertdata.blade.php
+++ resources/views/sale/insertdata.blade.php
@@ -171,7 +171,7 @@ billidInput.addEventListener('input', () => {
             <option value="บิล/PO3/วางบิล">บิล/PO3/วางบิล</option>
             <option value="บิล/PO3/วางบิล/สำเนาหน้าบิล2">บิล/PO3/วางบิล/สำเนาหน้าบิล2</option>
             <option value="บิล/PO3/สำเนาหน้าบิล2">บิล/PO3/สำเนาหน้าบิล2</option>
-            <option value="บิล/PO3/สำเนาหน้าบิล3">บิล/PO3/สำเนาหน้าบิล3</option>
+            <option value="บิล/PO5/สำเนาหน้าบิล3">บิล/PO5/สำเนาหน้าบิล3</option>
             <option value="บิล/PO3/บัญชี">บิล/PO3/บัญชี</option>
             <option value="ขายสด">ขายสด</option>
         </select>
