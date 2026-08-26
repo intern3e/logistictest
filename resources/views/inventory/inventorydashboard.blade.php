6113fdab58c45808dd772b6a5dcb2caee224203f
AIAgent <devintern3e@gmail.com>
2026-08-26 17:35:59 +0700

    ทดสอบ1

--- resources/views/inventory/inventorydashboard.blade.php
+++ resources/views/inventory/inventorydashboard.blade.php
@@ -223,6 +223,7 @@
     <div class="sb-sec">เมนูหลัก</div>
     <a class="sb-item" target="_blank" href="{{ route('inventory.transaction', ['create_by' => $authUser['name'] ?? '']) }}"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>รายการสินค้า เข้า-ออก</a>
     <a class="sb-item cur" target="_blank" href="{{ route('inventory.item', ['create_by' => $authUser['name'] ?? '']) }}"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z"/></svg>ค้นหาสินค้า</a>
+    <a class="sb-item cur" target="_blank" href="{{ route('inventory.vehicles', ['create_by' => $authUser['name'] ?? '']) }}"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z"/></svg>รถบริษัท</a>
   </div>
 </div>
 
